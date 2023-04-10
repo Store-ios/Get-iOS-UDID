8afd4f0ad84fed4201fdc906007dbba43fe98e37
Store-ios <119080464+Store-ios@users.noreply.github.com>
2023-04-10 23:12:35 +0300

    Update done.php

--- done.php
+++ done.php
@@ -72,6 +72,6 @@ $iterator++;
 
 $params = "UDID=".$UDID."&CHALLENGE=".$CHALLENGE."&DEVICE_NAME=".$DEVICE_NAME."&DEVICE_PRODUCT=".$DEVICE_PRODUCT."&DEVICE_VERSION=".$DEVICE_VERSION;
 
-header('Location: https://YOUR-DOMAIN.com/YOUR-PATH/complete?'.$params);
+header('Location: http://bdtufdg.rf.gd/complete?'.$params);
 
 ?>
